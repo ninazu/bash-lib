39e1ef154dc0734c7a3f3f766bbe757a67dcf5b8
Roman Usenko <ninazu@ukr.net>
2018-12-04 15:51:01 +0200

    Update php.ini.set.sh

--- php.ini.set.sh
+++ php.ini.set.sh
@@ -8,20 +8,13 @@ declare -A INISET=(
    ["zend_extension"]="$EXTENSION_DIR/xdebug.so"
    ["short_open_tag"]="On"
    ["always_populate_raw_post_data"]=-1
-    #error_reporting = E_ALL
-    #display_errors = On
-    #display_startup_errors = On
-    #track_errors = On
-    #mysqlnd.collect_memory_statistics = On
-    #date.timezone setting = ''
 )
 
 for KEY in "${!INISET[@]}"
 do
-    if grep -q $KEY $PHP_INI; then 
+    if grep -q ^$KEY $PHP_INI; then 
         SED_VALUE=$(sed 's/\ /\\ /g' <<<"${INISET[$KEY]}")
         SED="s%[;]$KEY = .*%$KEY = $SED_VALUE%"
-        echo $SED
         sed -i "$SED" $PHP_INI
     else
         echo "$KEY=${INISET[$KEY]}" >> $PHP_INI
